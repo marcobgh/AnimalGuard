b8d47efebea426492e444924bf62d863f974b13b
Marco Borghetti <75136675+marcobgh@users.noreply.github.com>
2024-11-26 22:15:07 -0300

    Update SistemaApplication.java

--- Back-end/AnimalGuard/sistema/sistema/src/main/java/com/animalguard/sistema/SistemaApplication.java
+++ Back-end/AnimalGuard/sistema/sistema/src/main/java/com/animalguard/sistema/SistemaApplication.java
@@ -2,12 +2,16 @@ package com.animalguard.sistema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@EntityScan(basePackages = "com.AnimalGuard.sistema.entity")
 @SpringBootApplication
-public class SistemaApplication {
+public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
